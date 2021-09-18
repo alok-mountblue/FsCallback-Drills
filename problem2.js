e7a2b1d156a9e9e45e867dce77fc332a7d70b508
alok <alok.sharma.17@mountblue.tech>
2021-09-18 16:12:01 +0530

    Update problem2 using promise

--- problem2.js
+++ problem2.js
@@ -1,48 +1,33 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const solve2 = (path) => {
-  fs.readFile(path, "utf8", (err, res) => {
-    if (err) console.log(err);
-    fs.writeFile("upperCase.txt", res.toUpperCase(), (err) => {
-      if (err) console.log(err);
-      fs.writeFile("filenames.txt", "upperCase.txt ", (err) => {
-        if (err) console.log(err);
-      });
-      fs.readFile("upperCase.txt", "utf8", (err, res) => {
-        if (err) console.log(err);
-        let lowerCase = res.toLowerCase().split(". ").join("\n");
-        fs.writeFile("lowerCase.txt", lowerCase, (err) => {
-          if (err) console.log(err);
-          fs.appendFile("filenames.txt", "lowerCase.txt ", (err) => {
-            if (err) console.log(err);
-          });
-          fs.readFile("lowerCase.txt", "utf8", (err, res) => {
-            if (err) console.log(err);
-            let sortData = res.split("\n").sort().join("\n");
-            fs.writeFile("sorted.txt", sortData, (err) => {
-              if (err) console.log(err);
-              fs.appendFile("filenames.txt", "sorted.txt", (err) => {
-                if (err) console.log(err);
-              });
-
-              fs.readFile("filenames.txt", "utf8", (err, res) => {
-                if (err) console.log(err);
-                //console.log(res);
-                let namesList = res.split(" "); //return array
-                //console.log(namesList);
-                for (let i = 0; i < namesList.length; i++) {
-                  fs.unlink(namesList[i], (err) => {
-                    //console.log(namesList[i]);
-                    if (err) console.log(err);
-                  });
-                }
-              });
-            });
-          });
-        });
-      });
+  fs.readFile(path, "utf8")
+    .then((res) => {
+      fs.writeFile("upperCase.txt", res.toUpperCase());
+      fs.writeFile("filenames.txt", "upperCase.txt ");
+      return fs.readFile("upperCase.txt", "utf8");
+    })
+    .then((res) => {
+      let lowerCase = res.toLowerCase().split(". ").join("\n");
+      fs.writeFile("lowerCase.txt", lowerCase);
+      fs.appendFile("filenames.txt", "lowerCase.txt ");
+      return fs.readFile("lowerCase.txt", "utf8");
+    })
+    .then((res) => {
+      let sortData = res.split("\n").sort().join("\n");
+      fs.writeFile("sorted.txt", sortData);
+      fs.appendFile("filenames.txt", "sorted.txt");
+      return fs.readFile("filenames.txt", "utf8");
+    })
+    .then((res) => {
+      let namesList = res.split(" ");
+      for (let i = 0; i < namesList.length; i++) {
+        fs.unlink(namesList[i]);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  });
 };
 
 module.exports = { solve2 };
